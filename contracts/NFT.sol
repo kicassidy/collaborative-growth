b9bf504394b803a86a5795948d4442847716a56a
gichiba <griffin@colony.io>
2019-02-16 13:43:21 -0700

    add basic NFT contract

---
+++ contracts/NFT.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+import "../lib/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+import "../lib/openzeppelin-solidity/contracts/token/ERC721/ERu721Metadata.sol";
+
+contract NFT is ERC721, ERC721Metadata {
+  constructor (string memory name, string memory symbol) public ERC721Metadata(name, symbol) {
+      // I don't think we're using solhint, but just in case:
+      // solhint-disable-previous-line no-empty-blocks
+    }
+}
