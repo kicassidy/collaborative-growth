95efb1289762b240f63ee4b8653f4fc3c0c77847
gichiba <griffin@colony.io>
2019-02-16 14:23:50 -0700

    override tokenURI function The new function takes a base string (which is a URL for hosted meta-data) and appends an Id, so that each token's meta-data can come from a simple hosted endpoint.

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,5 +7,17 @@ contract NFT is ERC721, ERC721Metadata {
   constructor (string memory name, string memory symbol) public ERC721Metadata(name, symbol) {
       // I don't think we're using solhint, but just in case:
       // solhint-disable-previous-line no-empty-blocks
-    }
+  }
+
+  function tokenURI(uint256 _tokenId) public view returns (string) {
+    return Strings.strConcat(
+        baseTokenURI(),
+        Strings.uint2str(_tokenId)
+    );
+  }
+
+  function baseTokenURI() public view returns (string) {
+    return "https://metadata-url/"; // wherever the metadata is hosted 
+  }
+
 }
