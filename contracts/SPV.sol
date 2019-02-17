f3154c9bfb233ea04799b49d26ab47884f49e638
gichiba <griffin@colony.io>
2019-02-16 22:04:22 -0700

    refactor and start on SPV.sol

--- contracts/SPV.sol
+++ contracts/SPV.sol
@@ -1,3 +1,27 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.4;
 
-contract SPV { ¯\_(ツ)_/¯ }
+import "../lib/dappsys-monolithic/token.sol";
+import "../lib/dappsys-monolithic/auth.sol";
+
+contract SPV is DSToken {
+
+  address public parentToken;
+  uint public parentTokenId;
+
+  constructor(address _parentToken, uint256 _parentTokenId, bytes32 _symbol,) public {
+        parentToken = _parentToken;
+        parentTokenId = _parentTokenId;
+        symbol = _symbol;
+  }
+
+  function parentToken() external view returns(address) {
+    return parentToken;
+  }
+
+  function parentTokenId() external view returns(uint256) {
+    return parentTokenId;
+  }
+
+
+
+}
--- migrations/2_contract_migration.js
+++ migrations/2_contract_migration.js
@@ -1,5 +1,7 @@
 var NonFungible = artifacts.require("./NFT.sol");
+var SpecialPurposeVehicle = artifacts.require("./SPV.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(NonFungible, "deed", "NFT");
+  deployer.deploy(SpecialPurposeVehicle, "OWN");
 };
